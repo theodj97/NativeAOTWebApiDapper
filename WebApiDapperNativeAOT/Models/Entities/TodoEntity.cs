c5f20e6dd511e6d3413898f0e128c23a5352683c
Theo <theo.delvas@es.gestamp.com>
2024-08-13 21:56:05 +0200

    Made folder Entities

--- WebApiDapperNativeAOT/Models/Entities/TodoEntity.cs
+++ WebApiDapperNativeAOT/Models/Entities/TodoEntity.cs
@@ -1,3 +1,3 @@
-﻿namespace WebApiDapperNativeAOT.Models;
+﻿namespace WebApiDapperNativeAOT.Models.Entities;
 
 public record TodoEntity(int Id, string Title, string? Description, int CreatedBy, string? AssignedTo = null, DateTime? TargetDate = null, bool IsComplete = false);
